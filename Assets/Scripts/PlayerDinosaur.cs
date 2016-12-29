971e826e2e4ed0e3e826cee54e47e2b6fa09cab2
David C. Page <evildavo@gmail.com>
2016-12-30 02:11:57 +1000

    The dinosaur now moves with the camera tracking

--- Assets/Scripts/PlayerDinosaur.cs
+++ Assets/Scripts/PlayerDinosaur.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class PlayerDinosaur : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    private Animator Animator;
+    private CameraTarget CameraTarget;
+
+    public float WalkSpeed = 1.0f;
+    public float RunSpeed = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+        Animator = GetComponent<Animator>();
+        CameraTarget = GetComponentInParent<CameraTarget>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {        
+        // Move forward based on the animation state. 
+        AnimatorStateInfo state = Animator.GetCurrentAnimatorStateInfo(0);
+  
+        if (state.IsName("Walk") || state.IsName("LowGrab") || state.IsName("HighGrab"))
+        {
+            CameraTarget.transform.Translate(WalkSpeed * Time.deltaTime, 0f, 0f);
+        }
+        else if (state.IsName("Run"))
+        {
+            CameraTarget.transform.Translate(RunSpeed * Time.deltaTime, 0f, 0f);
+        }
+    }
 }
